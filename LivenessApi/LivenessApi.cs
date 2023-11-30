f5b597ff3a8b162e4dad6b9197333df84a6e5c0e
gjlumsden <gjlumsden@gmail.com>
2023-11-30 16:46:40 +0000

    Add project files.

---
+++ LivenessApi/LivenessApi.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using Microsoft.ServiceFabric.Data;
+
+namespace LivenessApi
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance.
+    /// </summary>
+    internal sealed class LivenessApi : StatelessService
+    {
+        public LivenessApi(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        var builder = WebApplication.CreateBuilder();
+
+                        builder.Services.AddSingleton<StatelessServiceContext>(serviceContext);
+                        builder.WebHost
+                                    .UseKestrel()
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url);
+                        builder.Services.AddControllers();
+                        builder.Services.AddEndpointsApiExplorer();
+                        builder.Services.AddSwaggerGen();
+                        builder.Services.AddHealthChecks().AddCheck<HealthCheck>("HealthCheck");
+                        var app = builder.Build();
+                        if (app.Environment.IsDevelopment())
+                        {
+                        app.UseSwagger();
+                        app.UseSwaggerUI();
+                        }
+                        app.UseAuthorization();
+                        app.MapControllers();
+                        app.UseHealthChecks("/api/health-check");
+                        return app;
+
+                    }))
+            };
+        }
+    }
+}
